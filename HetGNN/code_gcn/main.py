e8b6ebfb64bf4ed0262a0665e3ec25784f2fa82e
Jiaxi Li <jiaxili0319@gmail.com>
2022-08-03 20:48:08 +1000

    resolve batch num train graph

--- HetGNN/code_gcn/main.py
+++ HetGNN/code_gcn/main.py
@@ -8,7 +8,7 @@ from train import Train
 @click.option('--save_model_freq', default=2, help='data_path')
 @click.option('--model_path', default='../model_save_clean', help='model path dir')
 @click.option('--data_path', default='../ProcessedData_clean', help='data path dir')
-@click.option('--num_train', default=65000, help='number of training graphs')
+@click.option('--num_train', default=64000, help='number of training graphs')
 @click.option('--num_eval', default=None, type=int, help='limit number of eval graphs, None for do not limit')
 @click.option('--batch_s', default=2000, help='batch size')
 @click.option('--mini_batch_s', default=500, help='mini batch size')
