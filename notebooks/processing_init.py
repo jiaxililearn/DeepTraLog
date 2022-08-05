c10b3c177f9a66b0aa3af5a421e7f59dfb5e9600
Jiaxi Li <jiaxili0319@gmail.com>
2022-08-05 10:34:21 +1000

    process init for eval notebooks

---
+++ notebooks/processing_init.py
@@ -0,0 +1,99 @@
+from tqdm import tqdm
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import pandas as pd
+from collections import defaultdict
+import numpy as np
+
+from sklearn.manifold import TSNE
+
+def loss_and_eval_results(model_result_root_dir, model_eval_freq=2):
+    train_loss = pd.read_csv(f'{model_result_root_dir}/train_loss.txt', names=['loss'])
+    train_loss = train_loss.reset_index().rename(columns={'index': 'epoch'})
+
+    eval_result = pd.read_csv(f'{model_result_root_dir}/eval_metrics.txt', sep=' ', names=['AUC', 'AP'])
+    eval_result = eval_result.reset_index().rename(columns={'index': 'epoch'})
+    eval_result['epoch'] = eval_result['epoch'] * model_eval_freq
+
+    eval_result['avg_metric'] = (eval_result['AUC'] + eval_result['AP'])/2
+
+
+    # train_loss, eval_result
+
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    for metric in eval_result.columns[1:]:
+        fig.add_trace(
+            go.Scatter(x=eval_result['epoch'], y=eval_result[metric],
+                       mode='lines',
+                       name=metric),
+            secondary_y=False
+        )
+
+    fig.add_trace(
+        go.Scatter(x=train_loss['epoch'], y=train_loss['loss'],
+                   mode='lines',
+                   name='loss'),
+        secondary_y=True,
+    )
+    fig.show()
+
+    return train_loss, eval_result
+
+
+def load_trace_info(data_root_dir):
+    return pd.read_csv(f'{data_root_dir}/trace_info.csv', index_col=None)
+
+
+def get_train_eval_test_gids(model_result_root_dir):
+    fname_train_list = 'model_gid_list_train.txt'
+    fname_eval_list = 'model_gid_list_eval.txt'
+    fname_test_list = 'model_gid_list_test.txt'
+
+    gid_list = []
+    for fname_lst in [fname_train_list, fname_eval_list, fname_test_list]:
+        with open(f'{model_result_root_dir}/{fname_lst}', 'r') as fin:
+            _list = [int(i) for i in fin.read().strip().split()]
+        gid_list.append(_list)
+    return gid_list
+
+
+def model_output(model, data):
+    graph_node_feature, graph_het_feature, graph_node_types = data
+    _out = model(data).detach().numpy()
+    _score = model.predict_score(data).cpu().detach().numpy()
+    return _out, _score
+
+def get_graph_results(data_root_dir, model, dataset, gid_list, name='train', num_feature=7):
+    g_embeddings = []
+    g_scores = []
+    for gid in tqdm(gid_list):
+        _embedding, _score = model_output(model, dataset[gid])
+        g_embeddings.append(_embedding)
+        g_scores.append(_score)
+    g_embeddings = np.array(g_embeddings)
+    
+    resultdf = pd.DataFrame(g_embeddings, columns=[f'e{i}' for i in range(num_feature)])
+    resultdf['scores'] = g_scores
+    resultdf['trace_id'] = gid_list
+    resultdf['dataset'] = name
+    trace_info_df = load_trace_info(data_root_dir)
+
+    resultdf = resultdf.merge(trace_info_df, on='trace_id', how='inner')
+    resultdf = add_tsne_embedding(resultdf, name=name)
+    return resultdf
+
+
+def add_tsne_embedding(resultdf, name='train', num_feature=7):
+    tsne_ = TSNE(n_components=2, init='random')
+    embeddings = resultdf[[f'e{i}' for i in range(num_feature)]]
+    tsne_embeddings = tsne_.fit_transform(embeddings)
+
+    resultdf['tsne_x'] = tsne_embeddings[:,0]
+    resultdf['tsne_y'] = tsne_embeddings[:,1]
+
+    fig = px.scatter(resultdf, x='tsne_x', y='tsne_y', color='trace_bool', title=f'{name} Graph Embeddings')
+    fig.show()
+
+    return resultdf
