2b04faa4b97a085ed647f72408444c8416f1d2d9
Jiaxi Li <jiaxili0319@gmail.com>
2022-08-07 13:56:48 +1000

    long type

--- HetGNN/code_gcn/GCN_3.py
+++ HetGNN/code_gcn/GCN_3.py
@@ -43,6 +43,8 @@ class HetGCN_3(nn.Module):
         forward propagate based on node features and edge index
         """
         x_node_feature, x_edge_index = data
+        if x_edge_index.dtype != torch.long:
+            x_edge_index = x_edge_index.type(torch.long)
         print(x_edge_index.dtype)
         h = self.conv1(x_node_feature, x_edge_index)
         h = h.tanh()
