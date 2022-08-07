30ebb8f2d65eab1b2621aa28c92286aab33bd77d
Jiaxi Li <jiaxili0319@gmail.com>
2022-08-07 14:35:03 +1000

    update bias parameters

--- HetGNN/code_gcn/HetGCNConv.py
+++ HetGNN/code_gcn/HetGCNConv.py
@@ -1,5 +1,6 @@
 from tqdm import tqdm
 import torch
+from torch.nn import Parameter
 from torch_geometric.nn import MessagePassing
 from torch_geometric.utils import add_self_loops, degree
 
@@ -7,6 +8,8 @@ class HetGCNConv(MessagePassing):
     def __init__(self, in_channels, out_channels):
         super(HetGCNConv, self).__init__(aggr='add')  # "Add" aggregation.
         self.lin = torch.nn.Linear(in_channels, out_channels)
+
+        self.bias = Parameter(torch.Tensor(out_channels))
 
     def forward(self, x, edge_index):
         # x has shape [num_nodes, in_channels]
@@ -25,7 +28,9 @@ class HetGCNConv(MessagePassing):
         x = self.lin(x)
 
         # Step 3-5: Start propagating messages.
-        return self.propagate(edge_index, size=(x.size(0), x.size(0)), x=x)
+        out = self.propagate(edge_index, size=(x.size(0), x.size(0)), x=x)
+        out += self.bias
+        return out
 
     def message(self, x_j, edge_index, size):
         # x_j has shape [num_edges, out_channels]
