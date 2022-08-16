8f688fbe8f75f2a827c6b766f5785e013577cd87
Jiaxi Li <jiaxili0319@gmail.com>
2022-08-17 09:25:11 +1000

    random seed

--- HetGNN/code_gcn/GCN_5.py
+++ HetGNN/code_gcn/GCN_5.py
@@ -11,7 +11,7 @@ class HetGCN_5(nn.Module):
         Het GCN based on HetGNN paper
         """
         super(HetGCN_5, self).__init__()
-        random_seed = 2
+        random_seed = 0
         torch.manual_seed(random_seed)
         torch.cuda.manual_seed_all(random_seed)
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
