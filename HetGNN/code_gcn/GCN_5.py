af7f2497174733d85518f4050c02c16acd27b949
Jiaxi Li <jiaxili0319@gmail.com>
2022-08-17 09:17:43 +1000

    seed update

--- HetGNN/code_gcn/GCN_5.py
+++ HetGNN/code_gcn/GCN_5.py
@@ -12,6 +12,7 @@ class HetGCN_5(nn.Module):
         """
         super(HetGCN_5, self).__init__()
         torch.manual_seed(10)
+        torch.cuda.manual_seed_all(10)
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.svdd_center = None
         self.model_path = model_path
