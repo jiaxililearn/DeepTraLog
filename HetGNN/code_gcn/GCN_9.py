aac38d22e416c81315eca9200da4c8e0fd62739b
Jiaxi Li <jiaxili0319@gmail.com>
2022-08-29 18:18:50 +1000

    sum dim

--- HetGNN/code_gcn/GCN_9.py
+++ HetGNN/code_gcn/GCN_9.py
@@ -77,7 +77,7 @@ class HetGCN_9(nn.Module):
             attn_w = self.attn(node_feature)
 
             # graph embedding
-            g_embedding = torch.sum(h * attn_w, 1).tanh()
+            g_embedding = torch.sum(h * attn_w, 0).tanh()
             print(f'g_embedding: {g_embedding.shape}')
             _out[i] = g_embedding
         return _out
