c5cc0b3a5952cd3cd3653ca4d51021911cc10650
Jiaxi Li <jiaxili0319@gmail.com>
2022-09-11 18:02:27 +1000

    debug

--- HetGNN/code_gcn/ppr.py
+++ HetGNN/code_gcn/ppr.py
@@ -38,13 +38,16 @@ class PPR:
 
         idx = scores.argsort()[::-1][: self.maxsize]
         neighbor = np.array(x.indices[idx])
+        print(f'a: {neighbor}')
 
         seed_idx = np.where(neighbor == seed)[0]
         if seed_idx.size == 0:
             neighbor = np.append(np.array([seed]), neighbor)
+            print(f'b: {neighbor}')
         else:
             seed_idx = seed_idx[0]
             neighbor[seed_idx], neighbor[0] = neighbor[0], neighbor[seed_idx]
+            print(f'c: {neighbor}')
 
         assert np.where(neighbor == seed)[0].size == 1
         assert np.where(neighbor == seed)[0][0] == 0
