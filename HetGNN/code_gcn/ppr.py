5ee82dcc155bbb6171c6cd552c5fd3c3f9420bfa
Jiaxi Li <jiaxili0319@gmail.com>
2022-09-11 18:08:02 +1000

    debug

--- HetGNN/code_gcn/ppr.py
+++ HetGNN/code_gcn/ppr.py
@@ -31,12 +31,14 @@ class PPR:
         x = sp.csc_matrix(
             (np.ones(1), ([seed], np.zeros(1, dtype=int))), shape=[self.P.shape[0], 1]
         )
+        print(f'a: {x.data}')
+
         r = x.copy()
         for _ in range(self.n_order):
             x = (1 - alpha) * r + alpha * self.P @ x
         scores = x.data / (self.d[x.indices] + 1e-9)
         
-        print(f'a: {x.data}')
+        
 
 
         idx = scores.argsort()[::-1][: self.maxsize]
