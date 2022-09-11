b357350b1691739216ad65942ac63ac424f36f2c
Jiaxi Li <jiaxili0319@gmail.com>
2022-09-11 19:27:01 +1000

    threading

--- HetGNN/code_gcn/generate_subgraph.py
+++ HetGNN/code_gcn/generate_subgraph.py
@@ -1,10 +1,13 @@
 import torch
+from tqdm import tqdm
 from cytoolz import curry
 from torch_geometric.data import Data, Batch
 from data_loader import HetGCNEventGraphDataset
 from subgraph import Subgraph
 from ppr import PPR
-import multiprocessing as mp
+
+# import multiprocessing as mp
+from threading import Thread
 
 
 class SubgraphGenerator(object):
@@ -20,17 +23,29 @@ class SubgraphGenerator(object):
             include_edge_type=True,
         )
 
-    def process_ppr(self, gid):
-        x, edge_index, (_, edge_type), node_types = self.dataset[gid]
-        ppr = PPR(gid, x, edge_index, n_order=10)
-        ppr.search_all(x.shape[0], self.ppr_path)
-    
-    def run(self):
-        with mp.Pool() as pool:
-            list(
-                pool.imap_unordered(self.process_ppr, list(range(1000)), chunksize=500)
-            )
+    def process_ppr(self, gids):
+        for gid in tqdm(gids):
+            x, edge_index, (_, edge_type), node_types = self.dataset[gid]
+            ppr = PPR(gid, x, edge_index, n_order=10)
+            ppr.search_all(x.shape[0], self.ppr_path)
 
-if __name__ == '__main__':
+    def run(self):
+        chunk_size = 500
+        num_nodes = 1000  # 132485
+        threads = [
+            Thread(target=self.process_ppr, args=(list(range(i, i + chunk_size))))
+            for i in range(0, num_nodes, chunk_size)
+        ]
+
+        # start the threads
+        for thread in threads:
+            thread.start()
+
+        # wait for the threads to complete
+        for thread in threads:
+            thread.join()
+
+
+if __name__ == "__main__":
     sub = SubgraphGenerator()
     sub.run()
