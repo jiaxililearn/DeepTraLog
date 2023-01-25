1d8b97082c1f62053ccd7b67fa0e2a70ad44164e
Jiaxi Li <jiaxili0319@gmail.com>
2023-01-25 15:09:15 +1100

    revert back

--- HetGNN/code_gcn/HetGCNConv_11.py
+++ HetGNN/code_gcn/HetGCNConv_11.py
@@ -41,7 +41,7 @@ class HetGCNConv_11(MessagePassing):
         elif self.ablation == 'no-edge-node-relation':
             self.num_edge_types = 1
             self.num_src_types = 1
-            # self.num_node_types = 1
+            self.num_node_types = 1
 
         # self.k = 12
         # first het node hidden layer
